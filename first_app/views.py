16a6200ab811cfcd9490ecc7df665a61b74e1311
zaid20485 <zaid.altameemi@gmail.com>
2020-01-01 19:13:03 +0300

    "firsd upload:"
    Signed-off-by: zaid20485 <zaid.altameemi@gmail.com>

---
+++ first_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from first_app.models import Webpage, Topic, AccessRecord
+
+# Create your views here.
+
+
+def index(request):
+    # webpage_list = AccessRecord.objects.order_by ('date')
+
+    date_dict = {'access_records':AccessRecord.objects.order_by('date')}
+    
+    return render(request,'first_app/index.html', context=date_dict)
+
